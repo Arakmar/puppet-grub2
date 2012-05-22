02103beb55c34385ee39a41a013e213d69911d8c
Yoann Laissus <yoann.laissus@gmail.com>
2012-05-22 11:38:57 +0200

    A new grub module

---
+++ manifests/init.pp
@@ -0,0 +1,83 @@
+class grub {
+
+	$grub_default = $grub_default ? {
+		""      => 0,
+		default => $grub_default
+	}
+
+	$grub_timeout = $grub_timeout ? {
+		""      => 5,
+		default => $grub_timeout
+	}
+
+	$grub_distributor = $grub_distributor ? {
+		""      => "`lsb_release -i -s 2> /dev/null || echo Debian`",
+		default => $grub_distributor
+	}
+
+	$grub_cmdline_linux_default = $grub_cmdline_linux_default ? {
+		""      => "quiet",
+		default => $grub_cmdline_linux_default
+	}
+
+	$grub_cmdline_linux = $grub_cmdline_linux ? {
+		""      => "",
+		default => $grub_cmdline_linux
+	}
+
+	$grub_badram = $grub_badram ? {
+		""      => false,
+		default => $grub_badram
+	}
+
+	$grub_terminal = $grub_terminal ? {
+		""      => false,
+		default => $grub_terminal
+	}
+	
+	$grub_serial_command = $grub_serial_command ? {
+		""      => false,
+		default => $grub_serial_command
+	}
+
+	$grub_gfxmode = $grub_gfxmode ? {
+		""      => false,
+		default => $grub_gfxmode
+	}
+
+	# In this case, false is the default value, not unset
+	$grub_disable_linux_uuid = $grub_disable_linux_uuid ? {
+		""      => false,
+		default => $grub_disable_linux_uuid
+	}
+
+	# In this case, false is the default value, not unset
+	$grub_disable_linux_recovery = $grub_disable_linux_recovery ? {
+		""      => false,
+		default => $grub_disable_linux_recovery
+	}
+
+	$grub_init_tune = $grub_init_tune ? {
+		""      => false,
+		default => $grub_init_tune
+	}
+
+	package { "grub-pc":
+		ensure => installed
+	}
+
+	file {
+		"/etc/default/grub":
+			content  => template("grub/grub.erb"),
+			owner   => root,
+			group   => root,
+			require => Package["grub-pc"],
+			mode    => 0644,
+			notify  => Exec["update_grub"];
+	}
+
+	exec { "update_grub":
+		command => "update-grub",
+		refreshonly => true
+	}
+}
