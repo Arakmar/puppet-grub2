7d70b398a1f5dd692286120007b376dc48f78fb4
Yoann Laissus <yoann.laissus@gmail.com>
2013-05-26 17:09:17 +0200

    Switch to paramterized class

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,67 +1,17 @@
-class grub2 {
-
-	$grub_default = $grub_default ? {
-		""      => 0,
-		default => $grub_default
-	}
-
-	$grub_timeout = $grub_timeout ? {
-		""      => 5,
-		default => $grub_timeout
-	}
-
-	$grub_distributor = $grub_distributor ? {
-		""      => "`lsb_release -i -s 2> /dev/null || echo Debian`",
-		default => $grub_distributor
-	}
-
-	$grub_cmdline_linux_default = $grub_cmdline_linux_default ? {
-		""      => "quiet",
-		default => $grub_cmdline_linux_default
-	}
-
-	$grub_cmdline_linux = $grub_cmdline_linux ? {
-		""      => "",
-		default => $grub_cmdline_linux
-	}
-
-	$grub_badram = $grub_badram ? {
-		""      => false,
-		default => $grub_badram
-	}
-
-	$grub_terminal = $grub_terminal ? {
-		""      => false,
-		default => $grub_terminal
-	}
-	
-	$grub_serial_command = $grub_serial_command ? {
-		""      => false,
-		default => $grub_serial_command
-	}
-
-	$grub_gfxmode = $grub_gfxmode ? {
-		""      => false,
-		default => $grub_gfxmode
-	}
-
-	# In this case, false is the default value, not unset
-	$grub_disable_linux_uuid = $grub_disable_linux_uuid ? {
-		""      => false,
-		default => $grub_disable_linux_uuid
-	}
-
-	# In this case, false is the default value, not unset
-	$grub_disable_linux_recovery = $grub_disable_linux_recovery ? {
-		""      => false,
-		default => $grub_disable_linux_recovery
-	}
-
-	$grub_init_tune = $grub_init_tune ? {
-		""      => false,
-		default => $grub_init_tune
-	}
-
+class grub2(
+        $grub_default = 0,
+        $grub_timeout = 5,
+        $grub_distributor = "`lsb_release -i -s 2> /dev/null || echo Debian`",
+        $grub_cmdline_linux_default = "quiet",
+        $grub_cmdline_linux = "",
+        $grub_badram = false,
+        $grub_terminal = false,
+        $grub_serial_command = false,
+        $grub_gfxmode = false,
+        $grub_disable_linux_uuid = false,
+        $grub_disable_linux_recovery = false,
+        $grub_init_tune = false,
+) {
 	package { "grub-pc":
 		ensure => installed
 	}
